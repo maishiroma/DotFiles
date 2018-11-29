8e822ce6c74161f348ee5d07595fa0e3e859094d
shiro105 <shiro105@mail.chapman.edu>
2018-11-28 22:37:47 -0800

    Added bash and zsh config files and added in bootstrap script

---
+++ bootstrap.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# This script installs the necessary components in using zsh and configuring it how it should be.
+
+# Installing Oh-My-Zsh
+# https://github.com/robbyrussell/oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# Installing ZSH autosuggestions
+# https://github.com/zsh-users/zsh-autosuggestions/blob/master/INSTALL.md
+
